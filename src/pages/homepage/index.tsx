3fa52b9da770799a2f41957593b2dae79b937541
kristianreyes7 <kristianreyes7@icloud.com>
2022-12-07 10:14:27 -0800

    add react bootstrap,

---
+++ src/pages/homepage/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+import Header from '../../components/header'
+
+export default function HomePage() {
+    return (
+        <>
+            <div>Home</div>
+        </>
+    )
+}
