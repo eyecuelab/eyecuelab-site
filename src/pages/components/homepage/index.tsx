303838b275958e95136769e28dd17cbe47eb4550
kristianreyes7 <kristianreyes7@icloud.com>
2022-12-09 15:24:26 -0800

    start on home

--- src/pages/components/homepage/index.tsx
+++ src/pages/components/homepage/index.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function HomePage() {
     return (
-        <div>
-            <div>Home</div>
+        <div className='text-5xl text-white h-screen relative pb-32 px-3 leading-relaxed w-[95vw] md:w-[40vw] md:ml-44 mb-20 ml-3 '>
+            <h3 className='absolute left-0 bottom-0'>Your Team for Truly Innovative <span className='font-bold'>Software Design + Development</span></h3>
         </div>
     )
 }
