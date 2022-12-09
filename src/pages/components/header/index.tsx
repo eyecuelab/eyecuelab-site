a5f7c4b74ce792f84061b81fbc4b739f23564bd6
kristianreyes7 <kristianreyes7@icloud.com>
2022-12-09 13:23:38 -0800

    fix breakpoint

--- src/pages/components/header/index.tsx
+++ src/pages/components/header/index.tsx
@@ -12,14 +12,14 @@ export default function Header() {
 
     return (
         <>
-            <div className={cn('grid grid-cols-3 md:grid-cols-12 sticky w-screen', {
+            <div className={cn('grid grid-cols-3 lg:grid-cols-12 sticky w-screen', {
                 "bg-nav-blue drop-shadow-none": nav,
                 "bg-transparent": !nav
             })}>
                 <span className='p-3 flex justify-items-center'>
                     <img src={logo} alt="EyeCueLab" />
                 </span>
-                <ul className='hidden md:col-span-11 md:grid md:grid-cols-11 md:items-center md:text-white md:whitespace-nowrap'>
+                <ul className='hidden lg:col-span-11 lg:grid lg:grid-cols-11 lg:items-center lg:text-white lg:whitespace-nowrap'>
                     {headerLinks.map((nav, idx) => {
                         const navKey = `${idx}_navKey`;
                         return (
@@ -31,17 +31,17 @@ export default function Header() {
                         )
                     })}
                 </ul>
-                <button className="md:hidden text-white px-6 py-2 whitespace-nowrap flex justify-center items-center">
+                <button className="lg:hidden text-white px-6 py-2 whitespace-nowrap flex justify-center items-center">
                     <p className={nav ? 'hidden' : 'border px-6 py-2'}>
                         Contact Us
                     </p>
                 </button>
-                <span className='md:hidden text-white flex justify-end items-center pr-3' onClick={handleClick}>
+                <span className='lg:hidden text-white flex justify-end items-center pr-3' onClick={handleClick}>
                     {!nav ? <MenuAlt4Icon className='w-5 hover:scale-95' /> : <XIcon className='w-5 hover:scale-95' />}
                 </span>
             </div>
             <div>
-                <ul className={!nav ? "hidden" : 'md:hidden absolute bg-gradient-to-t from-nav-light-blue to-nav-blue w-screen h-screen p-3 flex flex-col justify-start'}>
+                <ul className={!nav ? "hidden" : 'lg:hidden absolute bg-gradient-to-t from-nav-light-blue to-nav-blue w-screen h-screen p-3 flex flex-col justify-start'}>
                     {headerLinks.map((nav, idx) => {
                         const navKey = `${idx}_navKey`;
                         return <li className="last:mt-36 text-white last:text-center font-semibold last:py-2 last:px-4 last:border last:border-white-400 mt-11 first:mt-32 text-2xl" onClick={handleClose} key={navKey}>{nav.title}</li>
