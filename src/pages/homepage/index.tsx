b7c9114900f2c3c5a7e2ac77496773f64b38e6f6
Kristian Reyes <kristianreyes7@icloud.com>
2022-12-13 10:21:10 -0800

    mobile view home

--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function HomePage() {
   return (
-    <div className="relative text-5xl text-white leading-relaxed h-full eyecuebody">
-      <h3 className="absolute left-52 bottom-20 w-1/2">
+    <div className="relative text-3xl text-white leading-relaxed h-full eyecuebody">
+      <h3 className="absolute w-full bottom-36 left-0 px-4 lg:px-0 md:left-52 md:bottom-36 md:w-5/12">
         Your Team for Truly Innovative <span className="font-bold">Software Design + Development</span>
       </h3>
     </div>
