d2655774e702329528df05848609b9b0de6ca3aa
kristianreyes7 <kristianreyes7@icloud.com>
2022-12-20 13:24:25 -0800

    padding

--- app/routes/components/homepage/landing_section/index.tsx
+++ app/routes/components/homepage/landing_section/index.tsx
@@ -3,7 +3,7 @@ export default function LandingPage() {
     <div className="absolute inset-0 bg-[url('../../public/images/Video.png')] bg-cover">
       
 <div className="container grid grid-rows-2 grid-cols-2 h-full w-full mx-auto">
-      <div className="md:w-2/3 h-full col-start-1 row-start-2 col-span-2 flex justify-start items-end p-2">
+      <div className="md:w-2/3 h-full col-start-1 row-start-2 col-span-2 flex justify-start items-end p-3">
         <h3 className="w-3/4 text-5xl leading-relaxed text-white pb-10">
           Your Team for Truly Innovative <span className="font-bold">Software Design + Development</span>
         </h3>
