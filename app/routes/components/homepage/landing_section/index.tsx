a22b6842448075d2869a483b863fdd1db1374676
kristianreyes7 <kristianreyes7@icloud.com>
2022-12-20 13:26:11 -0800

    more spacing

--- app/routes/components/homepage/landing_section/index.tsx
+++ app/routes/components/homepage/landing_section/index.tsx
@@ -4,7 +4,7 @@ export default function LandingPage() {
       
 <div className="container grid grid-rows-2 grid-cols-2 h-full w-full mx-auto">
       <div className="md:w-2/3 h-full col-start-1 row-start-2 col-span-2 flex justify-start items-end p-3">
-        <h3 className="w-3/4 text-5xl leading-relaxed text-white pb-10">
+        <h3 className="md:w-3/4 text-5xl leading-relaxed text-white pb-10">
           Your Team for Truly Innovative <span className="font-bold">Software Design + Development</span>
         </h3>
       </div>
