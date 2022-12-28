4fecfbae4e9da64025deb4006090c30ea3a68b49
kristianreyes7 <kristianreyes7@icloud.com>
2022-12-27 16:12:13 -0800

    text-box

--- src/components/homepage/landing_section/index.tsx
+++ src/components/homepage/landing_section/index.tsx
@@ -3,8 +3,8 @@ export default function LandingPage() {
   return (
     <>
       <div className="absolute inset-0 bg-landingPage bg-cover bg-no-repeat flex items-end pb-12">
-        <div className="w-[95vw] lg:w-[80vw] mx-auto h-1/2 flex items-center lg:items-end ">
-          <h3 className="lg:w-1/2 text-[3rem] leading-relaxed text-white py-6">
+        <div className="w-[95vw] lg:w-[80vw] mx-auto h-1/2 flex items-center lg:items-end">
+          <h3 className="lg:w-4/5 xl:w-1/2 text-[3rem] leading-relaxed text-white py-6 ">
             Your Team for Truly Innovative <span className="font-semibold">Software Design + Development</span>
           </h3>
         </div>
