7b08771e8c7c9aa7fd48e357e29b8edbbda52568
kristianreyes7 <kristianreyes7@icloud.com>
2022-12-29 22:53:38 -0800

    shapes page desktop

--- src/components/homepage/shapes_section/index.tsx
+++ src/components/homepage/shapes_section/index.tsx
@@ -60,23 +60,22 @@ export default function ShapesSection() {
       </div>
       {/* desktop */}
       <div className="border w-full h-full grid grid-cols-2 grid-rows-2">
-        <div className="relative bg-[#0085FF] row-span-2 flex items-center">
-          {/* todo: fix this */}
-          <div className="">
-            <div className="p-12 mb-10">
+        <div className="bg-[#0085FF] row-span-2 flex items-center justify-end">
+          <div className="relative w-4/5 h-4/5 flex items-center mt-16">
+            <div className="absolute top-14 w-full">
               <img
                 src={shape1}
                 alt="plane"
-                className="border w-2/3"
+                className="w-96"
               />
             </div>
-            <div className="flex flex-col justify-start w-2/3 p-8">
+            <div className="flex flex-col justify-center w-2/3 h-full">
               <h1 className="text-4xl font-semibold leading-relaxed w-72">
                 Launch
                 <br />
                 Successful MVPs
               </h1>
-              <p className="mt-8 w-72 border">You bring the vision, we'll help you successfully take it to market through our proven approach.</p>
+              <p className="mt-8 w-72">You bring the vision, we'll help you successfully take it to market through our proven approach.</p>
             </div>
           </div>
         </div>
@@ -114,32 +113,4 @@ export default function ShapesSection() {
     </div>
   );
 }
-/*
-    <div className="bg-[#0085FF] w-full h-full flex justify-end items-center">
-      <div className="h-2/3 flex flex-col items-center justify-center">
-        <div className="p-12 mb-10"><img src={shape1} alt="plane" /></div>
-        <div className="flex flex-col justify-start w-2/3 p-8">
-          <h1 className="text-5xl font-semibold leading-relaxed">Launch<br />Successful MVPs</h1>
-          <p className="mt-8">You bring the vision, we'll help you successfully take it to market through our proven approach.</p>
-        </div>
-      </div>
-    </div>
-    <div className="w-full h-full flex flex-col">
-        <div className="relative h-1/2 bg-[#0076FF] flex justify-start items-center">
-        <div className=" ml-8 p-16 z-10 w-3/5 flex flex-col justify-start items-center">
-          <h1 className="text-5xl font-semibold leading-relaxed">Enhance existing features</h1>
-          <p className="mt-4">Modernize internal workflows + architecture to produce industry-leading solutions.</p>
-        </div>
-        <div className="absolute left-0 w-4/5 flex justify-end ml-8"><img src={shape2} alt="plane" className="pr-14"/></div>
-      </div>
-      <div className="relative h-1/2 bg-[#0047FB] flex justify-start items-center">
-        <div className=" ml-8 p-16 z-10 w-3/5 flex flex-col justify-start items-center">
-          <h1 className="text-5xl font-semibold leading-relaxed">Create New Innovative Products</h1>
-          <p className="mt-4">Shake up your industry by developing + launching your next big product with our team of innovation experts.</p>
-        </div>
-        <div className="absolute left-0 w-4/5 flex justify-end ml-8"><img src={shape3} alt="plane" /></div>
-      </div>
-    </div>
 
-
-*/
