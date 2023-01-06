3569c01e2662852a336d18a94fa2ad9cf478208e
kristianreyes7 <kristianreyes7@icloud.com>
2023-01-05 20:25:04 -0800

    use react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { useState } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Header from "./components/header";
 import HomePage from "./components/homepage";
 import "./styles/App.css";
 
 function App() {
+  const router = createBrowserRouter([{ path: "/", element: <HomePage /> }]);
+
   return (
     <div
       style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}
       className="bg-zinc-400 w-screen h-screen overflow-x-auto snap-y snap-normal capitalize"
     >
       <Header />
-      <HomePage />
+      <RouterProvider router={router} />
     </div>
   );
 }
